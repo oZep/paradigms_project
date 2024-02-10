38ef2829d76b6385a2cd1b9b63e2b82ccf70d12d
joeyissa <joeyissa59@gmail.com>
2024-02-09 22:42:10 -0500

    adding base code and files

---
+++ part1/SimilaritySearch.java
@@ -0,0 +1,16 @@
+package part1;
+
+public class SimilaritySearch {
+    public static void main(String[] args) {
+
+        if (args.length > 2 || args.length < 1) {
+            System.err.println("java SimilaritySearch <image> <dataset>");
+        }
+
+        String image = args[0];
+        String dataset = args[1];
+
+        
+    }
+
+}
