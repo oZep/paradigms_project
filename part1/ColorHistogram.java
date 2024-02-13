16d3d78f2b6de55a6e16d1fdfe30ecf90a21e761
joeyissa <joeyissa59@gmail.com>
2024-02-13 17:53:49 -0500

    done

--- part1/ColorHistogram.java
+++ part1/ColorHistogram.java
@@ -7,6 +7,9 @@ public class ColorHistogram {
      * A constructor that construct a ColorHistogram instance for a d-bit image
      */
     public ColorHistogram(int d) {
+
+
+        //// this just reduces the images in the file to that value
         
     }
 
@@ -16,6 +19,10 @@ public class ColorHistogram {
 
     public ColorHistogram(String filename) {
 
+        // this takes a file and computes all of the histogram values for each image
+        // if you go to 25.jpg.txt the value [255,255,255] (white) is the first int
+        // the second int is [255,255,254] etc
+
     }
 
     /*
@@ -24,12 +31,20 @@ public class ColorHistogram {
 
     public void setImage(ColorImage image) {
 
+        // this is the image you want to compare all histograms with
+
     }
 
     /*
      * A getHistogram method that returns the normalized histogram of the image
      */
     public double[] getHistogram() {
+
+        //  you gotta normalize the histogram in the getHistogram method
+        //(divide every value by the number of pixels in the image)
+
+
+
         double[] temp = {2.0,2.0};
         return temp;
     }
